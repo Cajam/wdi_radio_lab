f91a1e710c4aeba8ac7c8fbfb1db664ec92cf85e
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2015-12-18 10:09:23 -0500

    Can't get form to show up?

--- js/form.directive.js
+++ js/form.directive.js
@@ -1,13 +1,24 @@
 (function(){
   angular
   .module("songs")
-  .directive("songForm", function(){
+  .directive("songForm", [
+    "$state",
+    "SongFactory",
+   function($state, Song){
     return {
       templateUrl: 'js/_song_form.html',
       // replace: true,
       scope: {
-        song: '='
+        song: '=',
+        formType: '@'
+      },
+      link: function(scope){
+        scope.create = function(){
+          scope.song.$save(scope.song, function(song){
+            $state.go("songShow", song);
+          });
+        }
       }
     }
-  });
+  }]);
 })();
