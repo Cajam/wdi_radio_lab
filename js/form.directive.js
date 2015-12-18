440036f7909f4e8d090e8341c2e2bbc92ddf68d8
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2015-12-18 10:44:27 -0500

    forms doing better

--- js/form.directive.js
+++ js/form.directive.js
@@ -1,10 +1,15 @@
+"use strict";
+
 (function(){
   angular
   .module("songs")
   .directive("songForm", [
     "$state",
     "SongFactory",
-   function($state, Song){
+    SongFormDirectiveFunction
+  ]);
+
+   function SongFormDirectiveFunction(SongFactory, $state){
     return {
       templateUrl: 'js/_song_form.html',
       // replace: true,
@@ -15,10 +20,18 @@
       link: function(scope){
         scope.create = function(){
           scope.song.$save(scope.song, function(song){
-            $state.go("songShow", song);
+            $state.go("songIndex", {}, {reload: true});
           });
         }
+        scope.update = function(){
+          scope.song.$update({id: scope.song.id})
+        };
+      scope.delete = function(){
+        scope.song.$delete({id: scope.song.id}, function(){
+          $state.go("songIndex", {}, {reload: true});
+        });
       }
     }
-  }]);
-})();
+  }
+}
+}());
